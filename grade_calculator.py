f720479a08742651a23d129b8f2e62e1de3b94a6
Sutean Rutjanalard <sutean.r@zcomsec.com>
2020-09-29 14:36:56 +0700

    Show student code.

--- grade_calculator.py
+++ grade_calculator.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     print("Calculating grade")
     for student in get_students():
         grade = calculate_grade(student)
-        print("Grade = %s"%grade)
+        code = student['code']
+        print("%s Grade = %s"%(code, grade))
